f0a4cb0cbf17c54ce52b2988e317eb495a789595
IMac3 <IMac3>
2023-02-15 11:56:30 +0300

    Добавьте файлы проекта.

---
+++ PinchukovLobutev9-14/ClassHelper/EFClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PinchukovLobutev9_14.Db;
+
+namespace PinchukovLobutev9_14.ClassHelper
+{
+    public class EFClass
+    {
+        public static Entities context {get;} = new Entities();    
+    }
+}
---
+++ PinchukovLobutev9-14/Windows/RegPersWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PinchukovLobutev9_14.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для RegPersWindow.xaml
+    /// </summary>
+    public partial class RegPersWindow : Window
+    {
+        public RegPersWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBlock_MouseLeftButtonUpAuth(object sender, MouseButtonEventArgs e)
+        {
+            AuthorizationWindow authorizationWindow = new AuthorizationWindow();
+            authorizationWindow.Show();
+            this.Close();
+
+        }
+    
+        private void TextBlock_MouseLeftButtonUpReg(object sender, MouseButtonEventArgs e)
+        {
+            RegWindow regWindow = new RegWindow();
+            regWindow.Show();
+            this.Close();
+        }
+    }
+}
